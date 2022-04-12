712546878addb320818fb913983ca9d357d64d8a
Rodolfo Infantini Carneiro <rodolfo.carneiro@sensedia.com>
2022-04-12 18:53:51 -0300

    width of the input

--- Graph.js
+++ Graph.js
@@ -52,10 +52,6 @@ export default class Graph {
         }
     }
 
-    drawPoint(x, y) {
-        this.drawLine(x, y, x, y)
-    }
-
     drawLine(x1, y1, x2, y2) {
         this.ctx.lineWidth = 2
         const centerX = this.realWidth / 2
